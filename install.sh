c45aeead57629b8c82a155d6ae2766f5cfc60b4a
Andrew Stangl <andrewstangl@gmail.com>
2015-03-26 13:23:16 +0200

    make sure we clear down existing files in the skeleton, to make our update process smoother

--- install.sh
+++ install.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+## Purge existing files, to ensure we remove files as well
+find $HOME/.puppet/var/puppet-module -type f -delete
+## Primary checkout command:
 find skeleton -type f | git checkout-index --stdin --force --prefix="$HOME/.puppet/var/puppet-module/" --
