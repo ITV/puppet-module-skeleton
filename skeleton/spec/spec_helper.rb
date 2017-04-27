15f6296a0ef7a4934bce5df6ec78e602f9fee3ea
Andrew Stangl <andrewstangl@gmail.com>
2017-04-27 13:41:37 +0200

    update skel from modulesync

--- skeleton/spec/spec_helper.rb
+++ skeleton/spec/spec_helper.rb
@@ -1,5 +1,4 @@
 require 'puppetlabs_spec_helper/module_spec_helper'
-require 'hiera-puppet-helper'
 require 'yaml'
 
 def fixture_path
@@ -18,6 +17,10 @@ RSpec.configure do |c|
   # Enable colour in Jenkins
   c.color = true
   c.tty = true
+  c.strict_variables = true
+  c.parser = 'future'
+  c.template_dir = File.expand_path(File.join(__FILE__, '../fixtures/templates'))
+  c.hiera_config = File.expand_path(File.join(__FILE__, '../fixtures/hiera.yaml'))
   c.before do
     # avoid "Only root can execute commands as other users"
     Puppet.features.stubs(:root? => true)
@@ -32,27 +35,25 @@ RSpec.configure do |c|
   end
 end
 
-# use both rpsec and yaml backends .. see: https://github.com/bobtfish/hiera-puppet-helper#advanced
-shared_context "hieradata" do
-  let(:hiera_config) do
-    { :backends => ['rspec', 'yaml'],
-    :hierarchy => [
-      '%{fqdn}/%{calling_module}',
-      '%{calling_module}',
-      'common'],
-    :yaml => {
-      :datadir => File.join(fixture_path, 'hieradata') },
-    :rspec => respond_to?(:hiera_data) ? hiera_data : {} }
-  end
-end
-
 shared_context "facter" do
   let(:default_facts) {{
     :kernel => 'Linux',
     :osfamily => 'RedHat',
     :concat_basedir => '/dne',
     :operatingsystem => 'CentOS',
+    :operatingsystemrelease => '7.0',
+    :operatingsystemmajrelease => '7.2',
     :architecture => 'x86_64',
+    :kernelversion => '3.10.0',
+    :puppetversion => '4.8.1',
+    :location => 'local',
+    :sudoversion => '1.8.6p7',
     :path => '/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin',
+    :selinux => false,
+    :is_virtual => true,
+    :ipaddress => '10.10.10.10',
+    :ipaddress_lo => '127.0.0.1',
+    :product => 'product',
+    :env => 'test',
   }}
 end
